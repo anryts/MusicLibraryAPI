7ce5123f63fa7f395b4f3f4298c0adb4d1023830
Zertimond <91977198+Zertimond@users.noreply.github.com>
2023-01-04 22:22:56 +0200

    Zhmudenko added specifications for GenreController

--- Contollers/GenreController.cs
+++ Contollers/GenreController.cs
@@ -7,6 +7,9 @@ using MusicLibraryAPI.Models.Response;
 
 namespace MusicLibraryAPI.Controller;
 
+/// <summary>
+/// Controller for working with Genres
+/// </summary>
 [ApiController]
 [Route("api/genres")]
 [Produces("application/json")]
@@ -20,14 +23,25 @@ public class GenreController : ControllerBase
         _context = context;
         _mapper = mapper;
     }
-    
+
+    /// <summary>
+    /// Get all genres
+    /// </summary>
+    /// <response code="200">Return all genres</response>
     [HttpGet]
     public ActionResult<IEnumerable<GetGenreResponse>> GetGenres()
     {
         var genres = _context.Genres.ToList();
         return Ok(_mapper.Map<IEnumerable<GetGenreResponse>>(genres));
     }
-    
+
+    /// <summary>
+    /// Get genre with id
+    /// </summary>
+    /// <param name="id">id of genre you want to get</param>
+    /// <response code="200">Return genre</response>
+    /// <response code="400">If id is incorrect</response>
+    /// <response code="404">If id does not exist</response>
     [HttpGet("{id}")]
     public ActionResult<GetGenreResponse> GetGenre(int id)
     {
@@ -38,7 +52,12 @@ public class GenreController : ControllerBase
         }
         return Ok(_mapper.Map<GetGenreResponse>(genre));
     }
-    
+
+    /// <summary>
+    /// Create new genre
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns>Returns created genre</returns>
     [HttpPost]
     public ActionResult<GetGenreResponse> CreateGenre([FromBody] CreateGenreRequest request)
     {
@@ -47,7 +66,15 @@ public class GenreController : ControllerBase
         _context.SaveChanges();
         return CreatedAtAction(nameof(GetGenre), new { id = genre.Id }, _mapper.Map<GetGenreResponse>(genre));
     }
-    
+
+    /// <summary>
+    /// Update genre
+    /// </summary>
+    /// <param name="id">id of genre you want to update</param>
+    /// <param name="request"></param>
+    /// <response code="200">Updates genre</response>
+    /// <response code="400">If id is incorrect</response>
+    /// <response code="404">If id does not exist</response>
     [HttpPut("{id}")]
     public ActionResult<GetGenreResponse> UpdateGenre(int id, [FromBody] string request)
     {
@@ -61,7 +88,14 @@ public class GenreController : ControllerBase
         _context.SaveChanges();
         return Ok(_mapper.Map<GetGenreResponse>(genre));
     }
-    
+
+    /// <summary>
+    /// Delete genre
+    /// </summary>
+    /// <param name="id">id of genre you want to delete</param>
+    /// <response code="200">Deletes genre</response>
+    /// <response code="400">If id is incorrect</response>
+    /// <response code="404">If id does not exist</response>
     [HttpDelete("{id}")]
     public ActionResult DeleteGenre(int id)
     {
@@ -77,5 +111,5 @@ public class GenreController : ControllerBase
     }
     
     
-    
+
 }
